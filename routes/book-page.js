024cf87ec294cd8ae5df57ef2f0b0e4fb67bbd99
qsmit <qsmity@gmail.com>
2020-09-09 15:59:02 -0400

    adding scratch file to gitignore

--- routes/book-page.js
+++ routes/book-page.js
@@ -1,29 +1,26 @@
 const express = require('express');
 const { asyncHandler } = require('../utils');
 const { Book } = require('../db/models');
-const router = expres.Router()
+const router = express.Router()
 
 
 
 //   /books
-router.get('/:id(\\d+)', asyncHandler( async(req, res) => {
-    const bookId = parseInt(req.params.id, 10)
-    const book = await Book.findByPk(bookId, 
-        {
-            include: Genre
-        })
-    const {
-         id,
-         author,
-         title,
-         description,
-         genres
-        } = book
-        console.log()
-    res.render('book', {
+router.get('/:id(\\d+)', (req, res) => {
+    // const bookId = parseInt(req.params.id, 10)
+    // const book = await Book.findByPk(bookId, 
+    //     {
+    //         include: Genre
+    //     })
+    // const {
+    //      id,
+    //      author,
+    //      title,
+    //      description,
+    //      genres
+    //     } = book
+    //     console.log()
+    res.render('book')
+})
 
-
-    })
-}))
-
-module.exports= router
+module.exports = router
