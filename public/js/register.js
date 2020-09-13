9818c36432aa8dbeb40df5a287dacce31c492415
TJ Taylor <tjtaylorjr@gmail.com>
2020-09-13 08:51:30 -0400

    finished coding base functionality for genre selection persistence

--- public/js/register.js
+++ public/js/register.js
@@ -1,25 +1,106 @@
 // import { handleErrors } from './utils.js'
 
 // to obtain any information stored in local storage related to user's genre selections
-const genreInfo = JSON.parse(localStorage.getItem('genreInfo')) || {};
-// select all the checkboxes
-const genreSelections = document.querySelectorAll('genre-container__checkbox');
-// listen for a change on the boxes and iterate to update our key value pairs
-genreSelections.addEventListener("change", event => {
+// const genreInfo = JSON.parse(localStorage.getItem('genreInfo')) || {};
+// // select all the checkboxes
+// // const genreSelections = document.querySelectorAll('input[type="checkbox"]:checked'), values = [];
+// const genreSelections = document.querySelectorAll('.genre-container__checkbox'), values = [];
+// console.log(genreSelections);
+// // select  button
+// const selectAllButton = document.getElementById('genre-select-all-button');
+// // listen for a change on the boxes and iterate to update our key value pairs
+// function isChecked(checkbox) {
+//   return checkbox.checked === true;
+// }
 
-  genreSelections.forEach(() => {
+// let allChecked = () => genreSelections.length === genreSelections.filter(isChecked(checkbox));
+// console.
+// genreSelections.addEventListener("change", event => {
+
+//   genreSelections.forEach(() => {
+//     genreInfo[this.id] = this.checked;
+//   });
+//   // set new value of user genre selections in local storage
+//   localStorage.setItem("genreInfo", JSON.stringify(genreInfo));
+//   window.location.href = "user/shelves";
+// });
+
+// // when the page loads set the checkboxes
+// genreInfo.forEach((key, value) => {
+//   ('#' + key).checked = value;
+// });
+
+
+
+
+// var isChecked = document.getElementById('elementName').checked;
+// if (isChecked) { //checked
+//   //execute code here
+// } else { //unchecked
+//   //execute code here
+// }
+// or if you want whenever the checkbox check is changed
+
+// var checkbox = document.getElementById('elementName');
+// checkbox.addEventListener("change", functionname, false);
+
+// function functionname() {
+//   var isChecked = checkbox.checked;
+//   if (isChecked) { //checked
+
+//   } else { //unchecked
+
+//   }
+// }
+
+const genreInfo = JSON.parse(localStorage.getItem('genreInfo')) || {};
+const $checkboxes = $("#availableGenres :checkbox");
+const $selectAllButton = $("#genre-select-all");
+const $submitButton = $("#genre-submit")
+
+function allChecked() {
+  return $checkboxes.length === $checkboxes.filter(":checked").length;
+}
+
+function updateButtonStatus() {
+  $selectAllButton.text(allChecked() ? "Uncheck all" : "Check all");
+}
+
+function handleButtonClick() {
+  $checkboxes.prop("checked", allChecked() ? false : true)
+}
+
+function updateStorage() {
+  $checkboxes.each(function () {
     genreInfo[this.id] = this.checked;
   });
-  // set new value of user genre selections in local storage
+
+  genreInfo["buttonText"] = $selectAllButton.text();
   localStorage.setItem("genreInfo", JSON.stringify(genreInfo));
-  window.location.href = "user/shelves";
+}
+
+$selectAllButton.on("click", function () {
+  handleButtonClick();
+  updateButtonStatus();
+  updateStorage();
 });
 
-// when the page loads set the checkboxes
-genreInfo.forEach((key, value) => {
-  ('#' + key).checked = value;
+$checkboxes.on("change", function () {
+  updateButtonStatus();
+  updateStorage();
 });
 
+// On page load
+$.each(genreInfo, function (key, value) {
+  $("#" + key).prop('checked', value);
+});
+
+$selectAllButton.text(genreInfo["buttonText"]);
+
+$submitButton.on("click", function ( event ) {
+  event.preventDefault();
+  $(location).attr('href', '/user/shelves');
+})
 
 // const updateGenres = document.querySelector('.genre-container__form');
 
