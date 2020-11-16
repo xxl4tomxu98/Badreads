0238bde5e0dc3ea6a70e27ff05b40979fcb27df3
qsmit <qsmity@gmail.com>
2020-11-16 03:48:51 -0500

    increased size of font on landing page, fixed signup/login error display, fixed grid overflow hidden issues on booksearch page

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,10 +1,20 @@
+// import { sign } from "jsonwebtoken";
+
  export const handleErrors = async (err, errorContainerClass) => {
     if (err.status >= 400 && err.status < 600) {
       const errorJSON = await err.json();
+      const signupErrorContainer = document.querySelector('.signup-errors-container')
+      const loginErrorContainer = document.querySelector('.login-errors-container')
+      //clear error containers after each submission
+      signupErrorContainer.innerHTML = ''
+      loginErrorContainer.innerHTML = ''
+
+      //grab the intended errors container
       const errorsContainer = document.querySelector(errorContainerClass);
+
       let errorsHtml = [
         `
-          <div class="alert alert-danger">
+          <div>
               Something went wrong. Please try again.
           </div>
         `,
@@ -13,7 +23,7 @@
       if (errors && Array.isArray(errors)) {
         errorsHtml = errors.map(
           (message) => `
-            <div class="alert alert-danger fade show">
+            <div>
                 ${message}
             </div>
           `
