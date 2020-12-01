85c2c2518751f5eda94ab9b687dc2698d5660816
qsmit <qsmity@gmail.com>
2020-12-01 17:45:51 -0500

    cleaning up code

--- public/js/navBar.js
+++ public/js/navBar.js
@@ -2,7 +2,6 @@
 const logoutButton = document.querySelector('.logout__button');
 
 logoutButton.addEventListener('click', () => {
-    console.log('clicked logout')
     localStorage.removeItem("BADREADS_ACCESS_TOKEN");
     localStorage.removeItem("BADREADS_CURRENT_USER_ID");
     window.location.href = "/";
--- public/js/user-profile.js
+++ public/js/user-profile.js
@@ -73,7 +73,6 @@ export const populateUserGenres = async () => {
 const logoutButton = document.querySelector('.logout__button');
 
 logoutButton.addEventListener('click', () => {
-    console.log('clicked logout')
     localStorage.removeItem("BADREADS_ACCESS_TOKEN");
     localStorage.removeItem("BADREADS_CURRENT_USER_ID");
     window.location.href = "/";
